cb2f77119125dc32b58c675c3fb75e512295b48c
Tetsuya Shibao <shibao.tetsuya@ooi-sbr.page>
2022-06-08 21:10:18 +0900

    step2

--- src/lib/src-stack.ts
+++ src/lib/src-stack.ts
@@ -1,8 +1,7 @@
 import { aws_dynamodb, Duration, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
-import { DynamoGetItem, DynamoAttributeValue }  from 'aws-cdk-lib/aws-stepfunctions-tasks';
+import { DynamoGetItem, DynamoAttributeValue, DynamoUpdateItem }  from 'aws-cdk-lib/aws-stepfunctions-tasks';
 import { Construct } from 'constructs';
-import { StateMachine } from 'aws-cdk-lib/aws-stepfunctions';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Choice, Condition, Fail, JsonPath, Parallel, Pass, StateMachine } from 'aws-cdk-lib/aws-stepfunctions';
 
 
 export class SrcStack extends Stack {
@@ -19,13 +18,58 @@ export class SrcStack extends Stack {
 
   // Get DynamoDB for Step Functions 
   const getDdbItem = new DynamoGetItem(this, 'Get Ddb Item' , {
-      key: { articleID: DynamoAttributeValue.fromString('0001') },
+      key: { articleID: DynamoAttributeValue.fromString(JsonPath.stringAt('$.ArticleID')) },
       table: articleTable ,
       consistentRead: false ,
+      inputPath: '$.ArticleID' ,
     });
   
-  // Defination for Step Functions
-  const definition = getDdbItem;
+  // [ToDo]Translate Text
+
+  // Update DynamoDB for Translate Text on Step Functions
+  const updateDdbItemTranslate = new DynamoUpdateItem(this, 'Update Ddb Item for Traslate' , {
+    key: { articleID: DynamoAttributeValue.fromString(JsonPath.stringAt('$.Item.ArticleID.S')) },
+    table: articleTable ,
+    inputPath: '$.Item.ArticleID.S' ,
+    expressionAttributeValues: {
+      ':EnglishVersionRef': DynamoAttributeValue.numberFromString(JsonPath.stringAt('$.Result.TranslatedText')),
+    },
+    updateExpression: 'SET EnglishVersionRef = :EnglishVersionRef',
+    outputPath: '$.Result.TranslatedText'
+  });
+
+  // [ToDo]Speech-To-Text 
+
+  // Update DynamoDB for Translate Text on Step Functions
+  const updateDdbItemSpeech = new DynamoUpdateItem(this, 'Update Ddb Item for Speech' , {
+    key: { articleID: DynamoAttributeValue.fromString(JsonPath.stringAt('$.Item.ArticleID.S')) },
+    table: articleTable ,
+    inputPath: '$.Item.ArticleID.S' ,
+    expressionAttributeValues: {
+      ':S3URL': DynamoAttributeValue.numberFromString(JsonPath.stringAt('$.Result.SynthesisTask.OutputUri')),
+    },
+    updateExpression: 'SET S3URL = :S3URLRef',
+    outputPath: '$.Result.SynthesisTask.OutputUri'
+  });
+
+  // Parallel for Step Functions 
+  const parallel = new Parallel(this, 'Parallel')
+    .branch( updateDdbItemTranslate )
+    .branch( updateDdbItemSpeech )
+    ;
+
+  // Error for Step Functions 
+  const jobFailed = new Fail(this, 'Job Failed', {
+      cause: 'AWS Step Functions Job Failed',
+      error: 'DescribeJob returned FAILED',
+    });
+
+  // Definition for Step Functions
+  const definition = getDdbItem
+    .next( new Choice(this, 'Item is present')
+        .when(Condition.isPresent('$.Item'), parallel)
+        .otherwise(jobFailed)
+    );
 
   // Step Functions
   const simpleStateMachine  =  new StateMachine(this, 'h4b-stateMachine', {
