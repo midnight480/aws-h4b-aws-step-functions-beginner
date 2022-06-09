8d3d0e48507a9caa5f1c0430b9a3f69440151d26
Tetsuya Shibao <shibao.tetsuya@ooi-sbr.page>
2022-06-09 15:24:28 +0900

    step2 modify

--- src/lib/src-stack.ts
+++ src/lib/src-stack.ts
@@ -1,12 +1,14 @@
 import { aws_dynamodb, Duration, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
-import { DynamoGetItem, DynamoAttributeValue, DynamoUpdateItem }  from 'aws-cdk-lib/aws-stepfunctions-tasks';
+import { DynamoGetItem, DynamoAttributeValue, DynamoUpdateItem, CallAwsService }  from 'aws-cdk-lib/aws-stepfunctions-tasks';
 import { Construct } from 'constructs';
-import { Choice, Condition, Fail, JsonPath, Parallel, Pass, StateMachine } from 'aws-cdk-lib/aws-stepfunctions';
+import { Choice, Condition, Fail, JsonPath, Parallel, Pass, StateMachine, Wait, WaitTime } from 'aws-cdk-lib/aws-stepfunctions';
 
 
 export class SrcStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+  // [ToDo]S3 Bucket
 
   // DynamoDB
   const articleTable = new aws_dynamodb.Table(this, 'articleTable' , {
@@ -23,8 +25,23 @@ export class SrcStack extends Stack {
       consistentRead: false ,
       inputPath: '$.ArticleID' ,
     });
-  
-  // [ToDo]Translate Text
+
+  /*------------------ */
+
+  // Translate Text
+  const translateText = new CallAwsService(this, 'TranslateText', {
+    service: 'Translate' ,
+    action: 'translateText' ,
+    iamAction: 'translate:TranslateText' ,
+    iamResources: ['*'] ,
+    inputPath: '$.Item.Detail.S' ,
+    parameters: {
+      "SourceLanguageCode": "ja" ,
+      "TargetLanguageCode": "en",
+       "Text.$": "$.Item.Detail.S"
+    } ,
+    resultPath: '$.Result' ,
+  });
 
   // Update DynamoDB for Translate Text on Step Functions
   const updateDdbItemTranslate = new DynamoUpdateItem(this, 'Update Ddb Item for Traslate' , {
@@ -38,7 +55,28 @@ export class SrcStack extends Stack {
     outputPath: '$.Result.TranslatedText'
   });
 
-  // [ToDo]Speech-To-Text 
+  // Translate Tasks
+  const translateTasks = translateText
+    .next(updateDdbItemTranslate)
+  ;
+
+  /*------------------ */
+
+  // Speech-To-Text 
+  const speechSynthesis = new CallAwsService(this, 'SpeechSynthesis', {
+   service: 'polly' ,
+   action: 'startSpeechSynthesisTask' ,
+   iamAction: 'polly:startSpeechSynthesisTask' ,
+   iamResources: ['*'] ,
+   inputPath: '$.Item.Detail.S' ,
+   parameters: {
+    "OutputFormat": "mp3",
+    "OutputS3BucketName": "h4b-sf-output-shibao",
+    "Text.$": "$.Item.Detail.S",
+    "VoiceId": "Mizuki"
+    },
+   resultPath: '$.Result' ,
+  })
 
   // Update DynamoDB for Translate Text on Step Functions
   const updateDdbItemSpeech = new DynamoUpdateItem(this, 'Update Ddb Item for Speech' , {
@@ -52,17 +90,59 @@ export class SrcStack extends Stack {
     outputPath: '$.Result.SynthesisTask.OutputUri'
   });
 
-  // Parallel for Step Functions 
-  const parallel = new Parallel(this, 'Parallel')
-    .branch( updateDdbItemTranslate )
-    .branch( updateDdbItemSpeech )
+  // Get Speech Task State
+  const getSpeechTask = new CallAwsService(this, 'GetSpeechSynthesisTask', {
+    service: 'polly' ,
+    action: 'getSpeechSynthesisTask' ,
+    iamAction: 'polly:getSpeechSynthesisTask' ,
+    iamResources: ['*'] ,
+    inputPath: '$.Result.SynthesisTask.TaskId' ,
+    parameters: {
+      "TaskId.$": "$.Result.SynthesisTask.TaskId"
+    } ,
+    resultPath: '$.Result' ,
+  });
+
+  const wait4task = new Wait(this, 'Wait for Speech Synthesis', {
+    time: WaitTime.secondsPath('$.waitSeconds'),
+  });
+
+    /*------------------ */
+
+  const speechtask11 =  speechSynthesis
+    .next(getSpeechTask)
     ;
 
-  // Error for Step Functions 
+  const speechtask21 = wait4task
+    .next(getSpeechTask)
+  ;
+
+  const speechtask12 = speechtask11
+    .next( new Choice(this, 'Check for Speech Synthesis')
+      .when(Condition.stringEquals('$.Result.SynthesisTask.TaskStatus', 'completed'), updateDdbItemSpeech)
+      .otherwise( speechtask21 )
+   );
+
+  // Speech Tasks
+  const speechTasks = speechtask12
+    
+    /*------------------ */
+
+  // Parallel for Step Functions 
+  const parallel = new Parallel(this, 'Parallel')
+    .branch( translateTasks )
+    .branch( speechTasks )
+    ;
+
+   /*------------------ */
+
+   // Error for Step Functions 
   const jobFailed = new Fail(this, 'Job Failed', {
       cause: 'AWS Step Functions Job Failed',
       error: 'DescribeJob returned FAILED',
     });
+
+  /*------------------ */
 
   // Definition for Step Functions
   const definition = getDdbItem
@@ -70,6 +150,8 @@ export class SrcStack extends Stack {
         .when(Condition.isPresent('$.Item'), parallel)
         .otherwise(jobFailed)
     );
+
+  /*------------------ */
 
   // Step Functions
   const simpleStateMachine  =  new StateMachine(this, 'h4b-stateMachine', {
